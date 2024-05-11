a68ff2840cb13e4d200ab73af5948d76e29b1668
Arthur Malveste <arthurmielemalveste@gmail.com>
2024-05-11 14:24:35 -0300

    teste deploy

--- app.py
+++ app.py
@@ -43,20 +43,18 @@ def salvar_dados():
 def chat_response():
     # Retrieve user message from POST data
     user_message = request.form["message"]
-
     # Add prefix to the prompt
     prefixed_message = "Faça uma mensagem bonita e amigavel de um paragrafo só, responda a pergunta mais haja como se fosse meu amigo, dependendo da idade converce como se fosse da idade também, se for uma pessoa com 35 anos ou mais não opte por gírias e nem assuntos mais infantis, se a idade for extremamente grande pergunte como piada se esta idade é ralmente a dele. Olá meu nome é: " + nome_salvo + "e eu tenho:" + idade_salva + "responda essa pergunta:" + user_message
 
     response = chat.send_message(prefixed_message)
 
+    # Combine response lines into a single string
+    ai_response = response.text.replace('\n', ' ')
+     # Substitui quebras de linha por espaços
+
     conversation = []
-
     conversation.append(user_message)
-
-    response_lines = response.text.split('\n')
-
-    # Add chatbot response lines to the conversation list
-    conversation.extend(response_lines)
+    conversation.append(ai_response) # Adiciona a resposta completa do Gemini
 
     if idade_salva < "30":
         # Add emoji randomly
