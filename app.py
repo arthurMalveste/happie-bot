107763519d126bae004bf23227a10a2607a0d28b
Arthur Malveste <arthurmielemalveste@gmail.com>
2024-05-11 12:39:11 -0300

    idades extremamente grandes

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ def chat_response():
     user_message = request.form["message"]
     
     # Adicionar um prefixo ao prompt
-    prefixed_message = "Faça uma mensagem bonita e amigavel de um paragrafo só, responda a pergunta mais haja como se fosse meu amigo, dependendo da idade converce como se fosse da idade também, se for uma pessoa com 35 anos ou mais não opte por gírias e nem assuntos mais infantis, Olá meu nome é: " + nome_salvo + "e eu tenho:" + idade_salva + "responda essa pergunta:" + user_message
+    prefixed_message = "Faça uma mensagem bonita e amigavel de um paragrafo só, responda a pergunta mais haja como se fosse meu amigo, dependendo da idade converce como se fosse da idade também, se for uma pessoa com 35 anos ou mais não opte por gírias e nem assuntos mais infantis, se a idade for extremamente grande pergunte como piada se esta idade é ralmente a deel. Olá meu nome é: " + nome_salvo + "e eu tenho:" + idade_salva + "responda essa pergunta:" + user_message
 
     # Verificar se a mensagem do usuário é uma saudação
     if user_message.lower() in ["oi", "olá", "e aí", "olá amigo", "olá amiga"]:
@@ -66,6 +66,7 @@ def chat_response():
     # Adicionar emoji aleatório
         emoji = random.choice(emojis)
         conversation[-1] += " " + emoji
+
 
     return render_template("index.html", conversation=conversation)
 
